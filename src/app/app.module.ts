f5bc049a000f70e136a9b4ff60d4752ff279e74c
berghach <berghach.chemseddine@gmail.com>
2025-02-04 10:10:18 +0100

    REC-4 login and sign up components are set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsModule
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsModule
 
-import { AppComponent } from './app.component';
-import { RegisterComponent } from './auth/register/register.component'; // Import your RegisterComponent
-import { RouterOutlet } from '@angular/router';
+// import { AppComponent } from './app.component';
+// import { RegisterComponent } from './auth/register/register.component'; // Import your RegisterComponent
+// import { RouterModule, RouterOutlet } from '@angular/router';
+// import { LoginComponent } from './auth/login/login.component';
+// import { routes } from './app.routes';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    RegisterComponent,
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterOutlet,
-  ],
-  providers: [],
-  exports: [ RegisterComponent ],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     RegisterComponent,
+//     LoginComponent,
+//   ],
+//   imports: [
+//     BrowserModule,
+//     ReactiveFormsModule,
+//     RouterOutlet,
+//     RouterModule.forRoot(routes),
+//   ],
+//   providers: [
+//     RouterOutlet,
+//   ],
+//   exports: [ 
+//     RegisterComponent,
+//     LoginComponent,
+//   ],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
+  // standalone: false,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login Form Submitted', this.loginForm.value);
+      // Add your login logic here
+    }
+  }
 }
