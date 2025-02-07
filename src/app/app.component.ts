c07363450311a49b6d41365f028e3d2c77ebae9f
berghach <berghach.chemseddine@gmail.com>
2025-02-07 10:32:10 +0100

    fixing navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { LocalStorageService } from './services/local-storage.service';
 import { preRegisteredUsers } from './services/users';
 import { HashService } from './services/hash.service.service';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavBarComponent,
+    CommonModule
+  ],
   // standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'recycle-hub';
+  showNavBar: boolean = false;
 
   constructor(
     private localStorageService: LocalStorageService,
-    private hashService: HashService
+    private hashService: HashService,
+    private router: Router
   ) {}
 
   async ngOnInit() {
@@ -25,6 +34,11 @@ export class AppComponent {
       const hashedUsers = await this.hashPasswords(preRegisteredUsers);
       this.localStorageService.setItem('users', hashedUsers);
     }
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.showNavBar = this.shouldShowNavBar(event.urlAfterRedirects);
+    });
   }
   async hashPasswords(users: any[]): Promise<any[]> {
     const hashedUsers = [];
@@ -33,5 +47,10 @@ export class AppComponent {
       hashedUsers.push({ ...user, password: hashedPassword });
     }
     return hashedUsers;
+  }
+
+  shouldShowNavBar(url: string): boolean {
+    const hideNavBarRoutes = ['/login', '/register'];
+    return !hideNavBarRoutes.includes(url);
   }
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LoginService } from '../services/auth/login.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    CommonModule
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  showMobileMenu: boolean = false;
 
+  constructor(
+    private loginService: LoginService
+  ) { }
+
+  toggleMobileMenu(){
+    this.showMobileMenu = !this.showMobileMenu;
+  }
+
+  onClickLogout() {
+    this.loginService.logout();
+  }
 }
